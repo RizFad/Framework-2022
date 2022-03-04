dd03324642e57e6741c7f0c07fcb40ee44f38caa
= <=>
2022-03-04 19:51:43 +0700

    Framework Modul 3

--- Tugas 2/web-react/web-reactrizqy/src/index.js
+++ Tugas 2/web-react/web-reactrizqy/src/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Test from './Test';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
+import HelloComponent from './HelloComponent';
+import Login from './Login/Login';
+// import HelloComponent from './HelloComponent';
 
-const Hello = () =>{
-  return <p>Hallo Massszzzeeeehhhh</p>
-}
+// const Hello = () =>{
+//   return <p>Hallo Massszzzeeeehhhh</p>
+// }
 
-ReactDOM.render(<Hello/>,document.getElementById('root'));
+// ReactDOM.render(<Hello/>,document.getElementById('root'));
+
+// function HelloComponent() {
+//   return HelloComponent
+// }
+
+// const HelloComponent = () => HelloComponent
+
+// class StateFullComponent extends React.Component{
+//   render(){
+//     return<p>StateFullComponent</p>
+//   }
+// }
+
+ReactDOM.render(<Login />, document.getElementById('root'));
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
